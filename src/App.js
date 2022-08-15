d618cc58b26ed5d8912181225596841052c9cae5
richard483 <richard.william483@gmail.com>
2022-08-15 19:25:20 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import layer1 from './loading/layer1.png';
+import layer2 from './loading/layer2.png';
+import layer3 from './loading/layer3.png';
+import layer4 from './loading/layer4.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={layer1} className="loading-animation" id='layer1' alt="logo" />
+        <img src={layer2} className="loading-animation" id='layer2' alt="logo" />
+        <img src={layer3} className="loading-animation" id='layer3' alt="logo" />
+        <img src={layer4} className="loading-animation" id='layer4' alt="logo" />
       </header>
     </div>
   );
